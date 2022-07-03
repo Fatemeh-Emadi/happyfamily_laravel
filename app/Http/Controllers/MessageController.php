18e7e47a3f021dd9086a62ea6fc67c0653db3447
Fateme Emadi <fatemehemadi2021@gmail.com>
2022-07-04 01:13:24 +0430

    laravel

---
+++ app/Http/Controllers/MessageController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Message;
+
+class MessageController extends Controller
+{
+    function index(){
+        $messages=Message::all();
+        return view("welcome",[
+            "messages"=>$messages
+        ]);
+    }
+    function add(Request $request)
+    {
+      $message=new Message();
+      $message->name=$request["name"];
+      $message->text=$request["text"];
+      $message->save();
+      return redirect()->to("/");
+    }
+}
